8182303da5f74e8f2cf0e80da4ca5efc4845571c
Lucas Arisi <ramo0052@algonquinlive.com>
2020-01-13 16:41:28 -0500

    Doing participation 2 for MTM6302

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = 'Lucas Arisi'
+console.log(name)
+console.log(`Hello my name is ${name}.`)
+
+let number = 5
+let product = number * 2
+let finalValue = (product * 5) / number
+
+console.log(`The number ${number} multiplied by 2 is ${number * 2}.`)
+console.log(`The number multiplied by 5 is ${product * 5}.`)
+console.log(`The result divided by 5 is ${(product * 5) / number}.`)
+console.log(`The result minus 7 is ${finalValue - 7}.`)
